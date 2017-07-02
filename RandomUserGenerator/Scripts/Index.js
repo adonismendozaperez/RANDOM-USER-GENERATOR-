96f293aeef299a8b06a47b25ebff70f9b5a15f9d
Adonis <adonis.mendoza@outlook.com>
2017-07-02 00:04:21 -0400

    Add Project

---
+++ RandomUserGenerator/Scripts/Index.js
@@ -0,0 +1,18 @@
+﻿new Vue({
+    el: 'main',
+    mounted() {
+        this.cargarpersonas();
+    },
+    data: {
+        personas: []
+    },
+
+    methods: {
+        cargarpersonas() {
+            this.$http.get('https://randomuser.me/api/?results=1')
+                .then((respuesta) => {
+                    this.personas = respuesta.body.results;
+                });
+        },
+    },
+});
